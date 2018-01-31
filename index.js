94e67b1aa3dacd30d0a7a04c2c4bd082306fd4de
vladpeet <36010771+vladpeet@users.noreply.github.com>
2018-01-31 21:54:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('tRA8W1Qy62bOM4OonU6Yd8KBSA1a6zHp'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
